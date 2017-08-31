3a9bf3998e5ad52585677cf5e8c15d8775cacbfb
Nicolas Narbais <nicolas.narbais@activeeon.com>
2017-08-31 12:33:58 -0700

    Change dist folder to docs to comply with Github pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist/js')
+        path: path.resolve(__dirname, 'docs/js')
     },
     module: {
         loaders: [{
